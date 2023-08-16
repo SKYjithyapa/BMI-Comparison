8537b69d62b545a04397fdb89788d9842305d90f
SKYjithyapa <125755221+SKYjithyapa@users.noreply.github.com>
2023-08-16 16:44:14 +0530

    initial

---
+++ script.js
@@ -0,0 +1,17 @@
+var mass,hight,bmic,bmik,iskasun;
+
+mass = prompt("enter kasun`s mass : ");
+hight  = prompt("enter kasun s hight : ");
+
+bmik = hight/(hight**2);
+
+
+mass = prompt("enter chamal`s mass : ");
+hight  = prompt("enter chamal s hight : ");
+
+bmic = hight/(hight**2);
+
+
+iskasun = (bmik > bmic);
+alert(iskasun);
+
